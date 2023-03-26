09ca6f789a1928d9709d5e0715419ba737eb759c
InTTeY <123205539+InTTeY@users.noreply.github.com>
2023-03-26 20:41:49 +0300

    Add files via upload

---
+++ PW5_1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+namespace PW5_1
+{
+    class program
+    {
+        static void PetAnimal(IAnimal animal)
+        {
+            Console.WriteLine("Гладим питомца, а он издаёт звук: ");
+            animal.Voice();
+        }
+        static void Main()
+        {
+            List<IAnimal> myAnimals = new List<IAnimal>();
+            myAnimals.Add(new Dog());
+            myAnimals.Add(new Cat());
+            myAnimals.Add(new Owl());
+            myAnimals.Add(new Duck());
+            myAnimals.Add(new Mouse());
+
+            foreach (IAnimal animal in myAnimals)
+            {
+                PetAnimal(animal);
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+    interface IAnimal
+    {
+        void Voice();
+    }
+    class Dog : IAnimal 
+    { 
+        public void Voice() 
+        { 
+            Console.WriteLine("Гав!"); 
+        } 
+    }
+    class Cat : IAnimal 
+    { 
+        public void Voice() 
+        { 
+            Console.WriteLine("Мяу!"); 
+        } 
+    }
+    class Owl : IAnimal
+    {
+        private int GetCurrentTime()
+        {
+            try { return Convert.ToInt32(File.ReadAllText("current_time.txt")); }
+            catch { return 0; }
+        }
+        public void Voice()
+        {
+            int currentTime = GetCurrentTime();
+            if ((currentTime >= 8) && (currentTime <= 20))
+            {
+                Console.WriteLine("Тисе, я спю");
+            }
+            else { Console.WriteLine("Ух! Ух! Ух!"); }
+        }
+
+    }
+    class Duck : IAnimal
+    {
+        public void Voice()
+        {
+            Console.WriteLine("Кря-кря!");
+        }
+    }
+    class Mouse : IAnimal
+    {
+        public void Voice()
+        {
+            Console.WriteLine("Пи-пи-пи");
+        }
+    }
+}
---
+++ PW5_2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace PW5_1
+{
+    class program
+    {
+        static void Main()
+        {
+            List<IHello> Greeting = new List<IHello>();
+            Greeting.Add(new RUS());
+            Greeting.Add(new AZE());
+            Greeting.Add(new HRV());
+            Greeting.Add(new FRA());
+            Greeting.Add(new KAZ());
+
+            foreach (IHello language in Greeting)
+            {
+                language.SayHello();
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+    interface IHello
+    {
+        void SayHello();
+    }
+    class RUS : IHello
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Привет");
+        }
+    }
+    class AZE : IHello
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Salam");
+        }
+    }
+    class HRV : IHello
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Bok.");
+        }
+
+    }
+    class FRA : IHello
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Salut");
+        }
+    }
+    class KAZ : IHello
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Сaлем");
+        }
+    }
+}
